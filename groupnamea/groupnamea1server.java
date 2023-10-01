942d9ccc2c345bb2cfbfab70076a9205055f772a
Anthony Sap <115178622+asapor1@users.noreply.github.com>
2023-10-01 16:36:28 -0400

    Update groupnamea1server.java

--- groupnamea/groupnamea1server.java
+++ groupnamea/groupnamea1server.java
@@ -1,29 +1,56 @@
-import java.net.*;
-import java.io.*;
+import java.security.*;
+import java.security.spec.ECParameterSpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.security.interfaces.ECPublicKey;
 
-public class groupnamea1server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket ss = new ServerSocket(24501);
-        Socket s = ss.accept();
-        
-        DataInputStream din = new DataInputStream(s.getInputStream());
-        
-        // Generate key pairs (SPub, SPri)
-        // Exchange SPub with client
-        // Use ECDH to generate the shared derived key K
-        // Print "generated derived key"
-        
-        // Convert the derived key K into the password string P
-        // Print "password:" and P
-        // Use P as the password for AES GCM encryption and decryption
-        
-        String encryptedM = din.readUTF();
-        
-        // Decrypt encryptedM using AES GCM
-        String M = "";  // replace with actual decryption code
-        
-        System.out.println("message M: " + M);
-        
-        din.close();
-    }
+import javax.crypto.KeyAgreement;
+
+import java.util.*;
+import java.nio.ByteBuffer;
+import java.io.Console;
+
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
+import static javax.xml.bind.DatatypeConverter.parseHexBinary;
+
+public class ecdh {
+
+  public static void main(String[] args) throws Exception {
+    Console console = System.console();
+    // Generate ephemeral ECDH keypair
+    KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC");
+    kpg.initialize(256);
+    KeyPair kp = kpg.generateKeyPair();
+    byte[] ourPk = kp.getPublic().getEncoded();
+
+    // Display our public key
+    console.printf("Public Key: %s%n", printHexBinary(ourPk));
+
+    // Read other's public key:
+    byte[] otherPk = parseHexBinary(console.readLine("Other PK: "));
+
+    KeyFactory kf = KeyFactory.getInstance("EC");
+    X509EncodedKeySpec pkSpec = new X509EncodedKeySpec(otherPk);
+    PublicKey otherPublicKey = kf.generatePublic(pkSpec);
+
+    // Perform key agreement
+    KeyAgreement ka = KeyAgreement.getInstance("ECDH");
+    ka.init(kp.getPrivate());
+    ka.doPhase(otherPublicKey, true);
+
+    // Read shared secret
+    byte[] sharedSecret = ka.generateSecret();
+    console.printf("Shared secret: %s%n", printHexBinary(sharedSecret));
+
+    // Derive a key from the shared secret and both public keys
+    MessageDigest hash = MessageDigest.getInstance("SHA-256");
+    hash.update(sharedSecret);
+    // Simple deterministic ordering
+    List<ByteBuffer> keys = Arrays.asList(ByteBuffer.wrap(ourPk), ByteBuffer.wrap(otherPk));
+    Collections.sort(keys);
+    hash.update(keys.get(0));
+    hash.update(keys.get(1));
+
+    byte[] derivedKey = hash.digest();
+    console.printf("Final key: %s%n", printHexBinary(derivedKey));
+  }
 }
